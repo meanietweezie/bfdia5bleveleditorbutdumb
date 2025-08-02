66541910be422ac2f1725bc5a20ecb907090a616
meanietweezie <aabatteries21@gmail.com>
2025-08-01 23:19:05 -0400

    made some changes to the image-to-level command. yay!

--- src/events/slashcommands/image-to-level.js
+++ src/events/slashcommands/image-to-level.js
@@ -3,46 +3,55 @@ const path = require("node:path");
 const fs = require("node:fs");
 const sharp = require("sharp");
 const {Jimp,intToRGBA,colorDiff,RG} = require("jimp");
+const convert = require("color-convert");
 let parentPath = String(__dirname).slice(0,String(__dirname).lastIndexOf("src")+3)
 //imagemagick.identify.path = path.join(parentPath.replace("\\src",""),"node_modules","node-imagemagick","index.js");
 const validImageTypes = ["image/png", "image/jpeg", "image/webp"];
-function calculateColorDistance(col1, col2) {
-    const dx = col2.r - col1.r;
-    const dy = col2.g - col1.g;
-    const dz = col2.b - col1.b;
-    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+function calculateColorDistance(imgPix, tileCol) {
+    const dx = tileCol.h - imgPix.h;
+    const dy = tileCol.s - imgPix.s;
+    const dz = tileCol.v - imgPix.v;
+    if(tileCol.v > 10 && imgPix.v < 10) return 10000000
+    return Math.sqrt(dx * dx + (dy * dy + dz * dz)/3);
 }
-function calculateColorDistance2(col1, col2) {
-    return Math.abs(col1.r - col2.r) + Math.abs(col1.g - col2.g) + Math.abs(col1.b - col2.b);
+function calculateColorDistance2(imgPix, tileCol) {
+    return Math.abs(imgPix.r - tileCol.r) + Math.abs(imgPix.g - tileCol.g) + Math.abs(imgPix.b - tileCol.b);
 }
-const tileColors = {
-  "./": { "r": 209, "g": 19, "b": 19 },
-  ".¸": { "r": 255, "g": 51, "b": 0 },
-  ".¯": { "r": 163, "g": 90, "b": 90 },
-  ".7": { "r": 84, "g": 31, "b": 31 },
-  ".º": { "r": 255, "g": 0, "b": 0 },
-  ".¶": { "r": 255, "g": 160, "b": 8 },
-  ".»": { "r": 107, "g": 75, "b": 26 },
-  ".®": { "r": 122, "g": 99, "b": 61 },
-  ".€": { "r": 170, "g": 138, "b": 85 },
-  ".M": { "r": 240, "g": 192, "b": 0 },
-  ".N": { "r": 211, "g": 169, "b": 0 },
-  "/B": { "r": 234, "g": 212, "b": 84 },
-  ".o": { "r": 32, "g": 223, "b": 32 },
-  ".j": { "r": 26, "g": 176, "b": 26 },
-  ".´": { "r": 145, "g": 255, "b": 0 },
-  ".8": { "r": 41, "g": 130, "b": 19 },
-  ".9": { "r": 45, "g": 85, "b": 34 },
-  "/A": { "r": 255, "g": 102, "b": 102 },
-  ".a": { "r": 0, "g": 102, "b": 255 },
-  ".b": { "r": 0, "g": 81, "b": 202 },
-  ".w": { "r": 153, "g": 0, "b": 255 },
-  ".{": { "r": 83, "g": 47, "b": 104 },
-  ".q": { "r": 0, "g": 0, "b": 0 },
-  ".¼": { "r": 51, "g": 51, "b": 51 },
-  ".²": { "r": 80, "g": 80, "b": 80 },
-  //"..": { "r": 255, "g": 255, "b": 255},
-}
+
+
+const tileColors = [
+  { "sim": "./", "h": 0.0, "s": 90.91, "v": 81.96 },
+  { "sim": ".¯", "h": 0.0, "s": 44.79, "v": 63.92 },
+  { "sim": ".7", "h": 0.0, "s": 63.1, "v": 32.94 },
+  { "sim": ".º", "h": 0.0, "s": 100.0, "v": 100.0 },
+  { "sim": "/A", "h": 0.0, "s": 60.0, "v": 100.0 },
+  { "sim": ".q", "h": 0.0, "s": 0.0, "v": 0.0 },
+  { "sim": ".¼", "h": 0.0, "s": 0.0, "v": 20.0 },
+  { "sim": ".²", "h": 0.0, "s": 0.0, "v": 31.37 },
+  { "sim": "..", "h": 0.0, "s": 0.0, "v": 100.0 },
+  { "sim": ".¸", "h": 12.0, "s": 100.0, "v": 100.0 },
+  { "sim": ".»", "h": 36.3, "s": 75.7, "v": 41.96 },
+  { "sim": ".¶", "h": 36.92, "s": 96.86, "v": 100.0 },
+  { "sim": ".®", "h": 37.38, "s": 50.0, "v": 47.84 },
+  { "sim": ".€", "h": 37.41, "s": 50.0, "v": 66.67 },
+  { "sim": ".M", "h": 48.0, "s": 100.0, "v": 94.12 },
+  { "sim": ".N", "h": 48.06, "s": 100.0, "v": 82.75 },
+  { "sim": "/B", "h": 51.2, "s": 64.1, "v": 91.76 },
+  { "sim": ".´", "h": 85.88, "s": 100.0, "v": 100.0 },
+  { "sim": ".9", "h": 107.06, "s": 60.0, "v": 33.33 },
+  { "sim": ".8", "h": 108.11, "s": 85.38, "v": 50.98 },
+  { "sim": ".o", "h": 120.0, "s": 85.65, "v": 87.45 },
+  { "sim": ".j", "h": 120.0, "s": 85.23, "v": 69.02 },
+  { "sim": ".b", "h": 215.94, "s": 100.0, "v": 79.22 },
+  { "sim": ".a", "h": 216.0, "s": 100.0, "v": 100.0 },
+  { "sim": ".w", "h": 276.0, "s": 100.0, "v": 100.0 },
+  { "sim": ".{", "h": 277.89, "s": 54.81, "v": 40.78 },
+  { "sim": "./", "h": 330.0, "s": 90.91, "v": 81.96 },
+  { "sim": ".¯", "h": 330.0, "s": 44.79, "v": 63.92 },
+  { "sim": ".7", "h": 330.0, "s": 63.1, "v": 32.94 },
+  { "sim": ".º", "h": 330.0, "s": 100.0, "v": 100.0 },
+  { "sim": "/A", "h": 330.0, "s": 60.0, "v": 100.0 }
+]
 const successMessages = [
     "here's your Strig....enjoy...teehee...",
     "ok i did the Thig. yay",
@@ -110,7 +119,7 @@ module.exports = {
                                 }
                                 ifMaxed = " (maxed to 400 tiles)" + (width == 400 ? " long" : " tall" + ".");
                             }
-                            sharp(imgPath).resize(width, height, {fit:"fill"}).jpeg().toFile(path.join(parentPath,"newimg.jpeg")).then((data) => {
+                            sharp(imgPath).resize(width, height, {fit:"fill"}).png().toFile(path.join(parentPath,"newimg.png")).then(() => {
                                 console.log(`Image resized to ${width}x${height}`);
                                 let level = image.name + "\r\n";
                                 level += width + "," + height + ",00,00,H\r\n";
@@ -125,16 +134,18 @@ module.exports = {
                                                 level += "..";
                                             }else{
                                                 hex.a = 255;
+                                                const [h, s, v] = convert.default.rgb.hsv(hex.r, hex.g, hex.b);
+                                                let hsvPix = { h, s, v }
                                                 let closestColor = null;
                                                 let closestDiff = Infinity;
-                                                for(const [key, value] of Object.entries(tileColors)) {
-                                                    const diff = calculateColorDistance(hex, value);
+                                                tileColors.forEach(value => {
+                                                    const diff = calculateColorDistance(hsvPix, value);
                                                     if(diff < closestDiff) {
                                                         closestDiff = diff;
-                                                        closestColor = key;
+                                                        closestColor = value.sim;
                                                     }
                                                     
-                                                }
+                                                })
                                                 level += closestColor;
                                             }
                                         }
@@ -144,7 +155,7 @@ module.exports = {
                                     fs.unlink(imgPath, err => {
                                         if (err) console.error("Error deleting temporary image file:", err);
                                     });
-                                    fs.unlinkSync(path.join(parentPath,"newimg.jpeg"));
+                                    //fs.unlinkSync(path.join(parentPath,"newimg.jpeg"));
                                     const levelPath = path.join(parentPath, image.name + ".txt");
                                     fs.writeFile(levelPath, level, (err) => {
                                         if (err) {
@@ -166,6 +177,7 @@ module.exports = {
                                 console.error("Error resizing image:", err);
                                 interaction.editReply("woops something bad happened. try again maybe");
                             });
+                            
 
                         }).catch(err => {
                             console.error("Error reading image metadata:", err);
@@ -178,47 +190,7 @@ module.exports = {
                     fs.unlink(imageName);
                     console.error(`Error downloading image: ${err.message}`);
                 });
-                /*
-                fetch(image.attachment).then(res => {
-                    //console.log(res)
-                    if (!res.ok) {
-                        interaction.editReply("didn't work mb");
-                        return;
-                    }
-                    res.buffer().then(data => {
-                        const type = image.contentType.split("/")[1];
-                        const imgPath = path.join(parentPath, "temp_image."+type)
-                        fs.writeFile(imgPath, data, (erro) => {
-                            if(erro) {
-                                console.log("error writing image file:", erro);
-                                return;
-                            }
-                            interaction.editReply(`image ${image.name} received. processing...`);
-                            
-                            imagemagick.identify("https://cdn.discordapp.com/ephemeral-attachments/1400577156857335950/1400640101586964593/hallowe_n_3.png?ex=688d5f3e&is=688c0dbe&hm=677e936776b6d05bb179663b468b2ae7ba010fa45e0db8fb69670a281c07c912&", function(err, features) {
-                                if (err) {
-                                    console.log("Error identifying image:", err);
-                                    interaction.editReply("There was an error processing the image. Please try again with a valid image.");
-                                    return;
-                                }
-                                console.log(features);
-                            })
-                            
-                        })
-                    
-                    })
-                    
-                    if(sub == "customdim") {
-                        // Handle custom dimensions logic here
-                        interaction.editReply(`Image ${image.name} will be converted to a level with dimensions ${width}x${height}.`);
-                    } else if(sub == "scale") {
-                        interaction.editReply(`Image ${image.name} will be scaled down to ${scale}% of its original size.`);
-                    } else {
-                        interaction.editReply("Invalid subcommand.");
-                    }
-                    
-                })
-                */
+                
             }
         });
     }
