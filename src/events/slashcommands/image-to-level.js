29c066498223e9de3fcc36f9333d8d45da7c544d
meanietweezie <aabatteries21@gmail.com>
2025-08-03 23:55:44 -0400

    slight change to the image-to-level command. again! wow. i suck at this

--- src/events/slashcommands/image-to-level.js
+++ src/events/slashcommands/image-to-level.js
@@ -15,42 +15,41 @@ function calculateColorDistance(imgPix, tileCol) {
     return Math.sqrt(dx * dx + (dy * dy + dz * dz)/3);
 }
 function calculateColorDistance2(imgPix, tileCol) {
-    return Math.abs(imgPix.r - tileCol.r) + Math.abs(imgPix.g - tileCol.g) + Math.abs(imgPix.b - tileCol.b);
+    return Math.sqrt((imgPix.r - tileCol.r)**2 + (imgPix.g - tileCol.g)**2 + (imgPix.b - tileCol.b)**2);
 }
 
 
 const tileColors = [
-  { "sim": "./", "h": 0.0, "s": 90.91, "v": 81.96 },
-  { "sim": ".¯", "h": 0.0, "s": 44.79, "v": 63.92 },
-  { "sim": ".7", "h": 0.0, "s": 63.1, "v": 32.94 },
-  { "sim": ".º", "h": 0.0, "s": 100.0, "v": 100.0 },
-  { "sim": "/A", "h": 0.0, "s": 60.0, "v": 100.0 },
-  { "sim": ".q", "h": 0.0, "s": 0.0, "v": 0.0 },
-  { "sim": ".¼", "h": 0.0, "s": 0.0, "v": 20.0 },
-  { "sim": ".²", "h": 0.0, "s": 0.0, "v": 31.37 },
-  { "sim": "..", "h": 0.0, "s": 0.0, "v": 100.0 },
-  { "sim": ".¸", "h": 12.0, "s": 100.0, "v": 100.0 },
-  { "sim": ".»", "h": 36.3, "s": 75.7, "v": 41.96 },
-  { "sim": ".¶", "h": 36.92, "s": 96.86, "v": 100.0 },
-  { "sim": ".®", "h": 37.38, "s": 50.0, "v": 47.84 },
-  { "sim": ".€", "h": 37.41, "s": 50.0, "v": 66.67 },
-  { "sim": ".M", "h": 48.0, "s": 100.0, "v": 94.12 },
-  { "sim": ".N", "h": 48.06, "s": 100.0, "v": 82.75 },
-  { "sim": "/B", "h": 51.2, "s": 64.1, "v": 91.76 },
-  { "sim": ".´", "h": 85.88, "s": 100.0, "v": 100.0 },
-  { "sim": ".9", "h": 107.06, "s": 60.0, "v": 33.33 },
-  { "sim": ".8", "h": 108.11, "s": 85.38, "v": 50.98 },
-  { "sim": ".o", "h": 120.0, "s": 85.65, "v": 87.45 },
-  { "sim": ".j", "h": 120.0, "s": 85.23, "v": 69.02 },
-  { "sim": ".b", "h": 215.94, "s": 100.0, "v": 79.22 },
-  { "sim": ".a", "h": 216.0, "s": 100.0, "v": 100.0 },
-  { "sim": ".w", "h": 276.0, "s": 100.0, "v": 100.0 },
-  { "sim": ".{", "h": 277.89, "s": 54.81, "v": 40.78 },
-  { "sim": "./", "h": 330.0, "s": 90.91, "v": 81.96 },
-  { "sim": ".¯", "h": 330.0, "s": 44.79, "v": 63.92 },
-  { "sim": ".7", "h": 330.0, "s": 63.1, "v": 32.94 },
-  { "sim": ".º", "h": 330.0, "s": 100.0, "v": 100.0 },
-  { "sim": "/A", "h": 206.21, "s": 38.33, "v": 89.02 }
+  { "sim": "./", "h": 0.0, "s": 90.91, "v": 81.96, "r": 209, "g": 19, "b": 19 },
+  { "sim": ".¯", "h": 0.0, "s": 44.79, "v": 63.92, "r": 163, "g": 90, "b": 90 },
+  { "sim": ".7", "h": 0.0, "s": 63.1, "v": 32.94, "r": 84, "g": 31, "b": 31 },
+  { "sim": ".º", "h": 0.0, "s": 100.0, "v": 100.0, "r": 255, "g": 0, "b": 0 },
+  { "sim": ".q", "h": 0.0, "s": 0.0, "v": 0.0, "r": 0, "g": 0, "b": 0 },
+  { "sim": ".¼", "h": 0.0, "s": 0.0, "v": 20.0, "r": 51, "g": 51, "b": 51 },
+  { "sim": ".²", "h": 0.0, "s": 0.0, "v": 31.37, "r": 80, "g": 80, "b": 80 },
+  { "sim": "..", "h": 0.0, "s": 0.0, "v": 100.0, "r": 255, "g": 255, "b": 255 },
+  { "sim": ".¸", "h": 12.0, "s": 100.0, "v": 100.0, "r": 255, "g": 85, "b": 0 },
+  { "sim": ".»", "h": 36.3, "s": 75.7, "v": 41.96, "r": 107, "g": 88, "b": 41 },
+  { "sim": ".¶", "h": 36.92, "s": 96.86, "v": 100.0, "r": 255, "g": 170, "b": 3 },
+  { "sim": ".®", "h": 37.38, "s": 50.0, "v": 47.84, "r": 122, "g": 102, "b": 61 },
+  { "sim": ".€", "h": 37.41, "s": 50.0, "v": 66.67, "r": 170, "g": 142, "b": 85 },
+  { "sim": ".M", "h": 48.0, "s": 100.0, "v": 94.12, "r": 240, "g": 240, "b": 0 },
+  { "sim": ".N", "h": 48.06, "s": 100.0, "v": 82.75, "r": 211, "g": 211, "b": 0 },
+  { "sim": "/B", "h": 51.2, "s": 64.1, "v": 91.76, "r": 234, "g": 229, "b": 88 },
+  { "sim": ".´", "h": 85.88, "s": 100.0, "v": 100.0, "r": 134, "g": 255, "b": 0 },
+  { "sim": ".9", "h": 107.06, "s": 60.0, "v": 33.33, "r": 54, "g": 85, "b": 34 },
+  { "sim": ".8", "h": 108.11, "s": 85.38, "v": 50.98, "r": 18, "g": 130, "b": 37 },
+  { "sim": ".o", "h": 120.0, "s": 85.65, "v": 87.45, "r": 32, "g": 223, "b": 32 },
+  { "sim": ".j", "h": 120.0, "s": 85.23, "v": 69.02, "r": 26, "g": 176, "b": 26 },
+  { "sim": ".b", "h": 215.94, "s": 100.0, "v": 79.22, "r": 0, "g": 47, "b": 202 },
+  { "sim": ".a", "h": 216.0, "s": 100.0, "v": 100.0, "r": 0, "g": 51, "b": 255 },
+  { "sim": ".w", "h": 276.0, "s": 100.0, "v": 100.0, "r": 204, "g": 0, "b": 255 },
+  { "sim": ".{", "h": 277.89, "s": 54.81, "v": 40.78, "r": 105, "g": 47, "b": 104 },
+  { "sim": "./", "h": 330.0, "s": 90.91, "v": 81.96, "r": 209, "g": 19, "b": 104 },
+  { "sim": ".¯", "h": 330.0, "s": 44.79, "v": 63.92, "r": 163, "g": 90, "b": 118 },
+  { "sim": ".7", "h": 330.0, "s": 63.1, "v": 32.94, "r": 84, "g": 31, "b": 55 },
+  { "sim": ".º", "h": 330.0, "s": 100.0, "v": 100.0, "r": 255, "g": 0, "b": 128 },
+  { "sim": "/A", "h": 206.21, "s": 60.0, "v": 100.0, "r": 102, "g": 170, "b": 255 }
 ]
 const successMessages = [
     "here's your Strig....enjoy...teehee...",
@@ -70,12 +69,12 @@ module.exports = {
             .addAttachmentOption(option => option.setName("image").setDescription("Image to convert to a level.").setRequired(true))
             .addIntegerOption(option => option.setName("width").setDescription("Width of the level in tiles.").setMinValue(1).setMaxValue(400).setRequired(true))
             .addIntegerOption(option => option.setName("height").setDescription("Height of the level in tiles.").setMinValue(1).setMaxValue(400).setRequired(true))
-            
+            .addBooleanOption(option => option.setName("huefocus").setDescription("Toggles on hue-saturation-value comparing rather than RGB comparing, often improving color matching.").setRequired(false))
         ).addSubcommand(
             subcommand => subcommand.setName("scale").setDescription("Scale down the image chosen, retaining the aspect ratio.")
             .addAttachmentOption(option => option.setName("image").setDescription("Image to convert to a level.").setRequired(true))
             .addIntegerOption(option => option.setName("scale").setDescription("Percentage of original dimensions.").setMinValue(1).setMaxValue(100).setRequired(true))
-            
+            .addBooleanOption(option => option.setName("huefocus").setDescription("Toggles on hue-saturation-value comparing rather than RGB comparing, often improving color matching.").setRequired(false))
         ),
     
     async execute(interaction) { 
@@ -86,6 +85,7 @@ module.exports = {
             let width = interaction.options.getInteger("width");
             let height = interaction.options.getInteger("height");
             let scale = interaction.options.getInteger("scale");
+            let hueFocus = interaction.options.getBoolean("huefocus");
             if(!validImageTypes.includes(image.contentType)) {
                 interaction.editReply("this isn't the right type of image bro use a png. or a jpeg. or a webp. yeah. maybe i'll add more later idk");
             }else{
@@ -133,19 +133,30 @@ module.exports = {
                                             if(hex.a < 30) {
                                                 level += "..";
                                             }else{
-                                                hex.a = 255;
-                                                const [h, s, v] = convert.default.rgb.hsv(hex.r, hex.g, hex.b);
-                                                let hsvPix = { h, s, v }
                                                 let closestColor = null;
-                                                let closestDiff = Infinity;
-                                                tileColors.forEach(value => {
-                                                    const diff = calculateColorDistance(hsvPix, value);
-                                                    if(diff < closestDiff) {
-                                                        closestDiff = diff;
-                                                        closestColor = value.sim;
-                                                    }
-                                                    
-                                                })
+                                                if(hueFocus) {
+                                                    const [h, s, v] = convert.rgb.hsv(hex.r, hex.g, hex.b);
+                                                    let hsvPix = { h, s, v }
+                                                    let closestDiff = Infinity;
+                                                    tileColors.forEach(value => {
+                                                        const diff = calculateColorDistance(hsvPix, value);
+                                                        if(diff < closestDiff) {
+                                                            closestDiff = diff;
+                                                            closestColor = value.sim;
+                                                        }
+                                                        
+                                                    })
+                                                }else{
+                                                    let closestDiff = Infinity;
+                                                    tileColors.forEach(value => {
+                                                        const diff = calculateColorDistance2(hex, value);
+                                                        if(diff < closestDiff) {
+                                                            closestDiff = diff;
+                                                            closestColor = value.sim;
+                                                        }
+                                                        
+                                                    })
+                                                }
                                                 level += closestColor;
                                             }
                                         }
